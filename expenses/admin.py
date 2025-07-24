0e4fad828a6a75a4678b7bc94edd660ec8909dbe
Daniel Erick <DanielErick.estudante@gmail.com>
2025-07-24 17:46:25 -0300

    feat(expenses): create full initial setup with categories, expenses, filters and API endpoints

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,29 +1,26 @@
 from django.contrib import admin
-from expenses.models import Expense, TypeOfExpense
+from .models import Category, Expense
 
 
-@admin.register(TypeOfExpense)
-class TypeOfExpenseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'user')
-    search_fields = ('name',)
-    list_filter = ('user',)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'name', )
+    search_fields = ('name', 'user__username', )
+    ordering = ('name', )
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
         queryset = queryset.filter(user=request.user)
         return queryset
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "type_of_expense":
-            kwargs["queryset"] = TypeOfExpense.objects.filter(user=request.user)
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 @admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type_of_expense', 'total_value', 'payment_status', 'user', )
-    list_filter = ('payment_status', 'type_of_expense', 'user', )
-    search_fields = ('name', )
+    list_display = ('id', 'name', 'amount', 'due_date', 'category', 'paid', 'user', )
+    search_fields = ('name', 'category__name', 'user__username', )
+    list_filter = ('paid', 'due_date', 'category', 'user')
+    ordering = ('-due_date', )
+    list_editable = ('paid', )
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
@@ -31,6 +28,6 @@ class ExpenseAdmin(admin.ModelAdmin):
         return queryset
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "type_of_expense":
-            kwargs["queryset"] = TypeOfExpense.objects.filter(user=request.user)
+        if db_field == 'category':
+            kwargs["queryset"] = Category.objects.filter(user=request.user)
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
--- expenses/models.py
+++ expenses/models.py
@@ -2,75 +2,55 @@ from django.db import models
 from costumers.models import CustomUser
 
 
-class TypeOfExpense(models.Model):
-    name = models.CharField(
-        max_length=100,
-        verbose_name='Nome do Tipo de Despesa'
-    )
+class Category(models.Model):
     user = models.ForeignKey(
         CustomUser,
         on_delete=models.CASCADE,
-        related_name='expense_types',
-        null=True,
-        blank=True,
+        related_name='expense_categories',
         verbose_name='Usuário'
     )
+    name = models.CharField(max_length=100, verbose_name='Nome')
 
     class Meta:
-        verbose_name = 'Tipo de Despesa'
-        verbose_name_plural = 'Tipos de Despesas'
-        unique_together = ('name', 'user')
+        verbose_name = 'Categoria De Despesa'
+        verbose_name_plural = 'Categorias De Despesas'
+        ordering = ['name']
+        unique_together = ('user', 'name')
 
     def __str__(self):
         return self.name
 
 
 class Expense(models.Model):
-    PAYMENT_STATUS = [
-        ('paid', 'pago'),
-        ('pending', 'pendente'),
-    ]
-    name = models.CharField(
-        max_length=100,
-        verbose_name='Nome da Despesa'
-    )
-    type_of_expense = models.ForeignKey(
-        TypeOfExpense,
-        on_delete=models.CASCADE,
-        verbose_name='Tipo de Despesa'
-    )
-    total_value = models.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-        verbose_name='Valor Total'
-    )
-    payment_status = models.CharField(
-        max_length=7,
-        choices=PAYMENT_STATUS,
-        default='pending',
-        verbose_name='Status de Pagamento'
-    )
     user = models.ForeignKey(
         CustomUser,
         on_delete=models.CASCADE,
-        verbose_name='Usuário',
-        related_name='expenses'
+        related_name='expenses',
+        verbose_name='Usuário'
     )
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Data de Criação'
-    )
-    is_installment = models.BooleanField(
-        default=False,
-        verbose_name='Parcelado',
+    name = models.CharField(max_length=255, verbose_name='Nome da Despesa')
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Valor')
+    due_date = models.DateField(verbose_name='Data de Vencimento / Referência')
+    payment_date = models.DateField(
+        verbose_name='Data de Pagamento',
         null=True,
         blank=True
     )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='expenses',
+        null=True,
+        blank=True
+    )
+    paid = models.BooleanField(default=False, verbose_name='Pago')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = 'Despesa'
-        verbose_name_plural = 'Despesas'
-        ordering = ['-created_at']
+        verbose_name_plural = 'despesas'
+        ordering = ['due_date']
 
     def __str__(self):
-        return f"{self.name} ({self.get_payment_status_display()})"
+        return self.name
